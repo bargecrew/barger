8ea5bdb69fd7cf3e773098a1346b45f22721ad8a
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2021-01-31 19:48:56 +0000

    auth

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,20 @@ use dotenv::dotenv;
 use std::env;
 
 async fn validator(req: actix_web::dev::ServiceRequest, _credentials: actix_web_httpauth::extractors::bearer::BearerAuth) -> Result<actix_web::dev::ServiceRequest, actix_web::Error> {
+    println!("{:?}", _credentials);
     Ok(req)
 }
 
 #[actix_web::get("/api/status")]
 async fn status() -> actix_web::HttpResponse {
+    let version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION must be set");
+    actix_web::HttpResponse::Ok().json(models::responses::GetStatusResponse{
+        version: version,
+    })
+}
+
+#[actix_web::get("/api/auth")]
+async fn auth() -> actix_web::HttpResponse {
     let version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION must be set");
     actix_web::HttpResponse::Ok().json(models::responses::GetStatusResponse{
         version: version,
@@ -32,9 +41,13 @@ async fn main() -> std::io::Result<()> {
     actix_web::HttpServer::new(|| {
         actix_web::App::new()
             .wrap(actix_web::middleware::Logger::default())
-            .wrap(actix_web_httpauth::middleware::HttpAuthentication::bearer(validator))
             .wrap(actix_cors::Cors::default())
             .service(status)
+            .service(
+                actix_web::web::scope("")
+                    .wrap(actix_web_httpauth::middleware::HttpAuthentication::bearer(validator))
+                    .service(auth)
+            )
     })
     .bind(format!("{host}:{port}", host = host, port = port))?
     .run()
