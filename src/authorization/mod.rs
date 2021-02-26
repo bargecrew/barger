516db2c4b9629941e37de3a2e643772c561a372a
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2021-02-26 21:54:54 +0000

    JWT

--- src/authorization/mod.rs
+++ src/authorization/mod.rs
@@ -1,7 +1,13 @@
+mod claims;
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use std::env;
+
 pub async fn authorize(
     req: actix_web::dev::ServiceRequest,
     credentials: actix_web_httpauth::extractors::bearer::BearerAuth,
 ) -> Result<actix_web::dev::ServiceRequest, actix_web::Error> {
+    let key = env::var("JWT_KEY").expect("JWT_KEY must be set");
+    let claims = get_claims(credentials.token(), &key);
     eprintln!("{:?}", credentials);
     match req.path() {
         "/api/users" => match req.method().as_str() {
@@ -14,4 +20,14 @@ pub async fn authorize(
             return Err(actix_web::error::ErrorNotFound("Not Found"));
         }
     }
+}
+
+fn get_claims(token: &str, key: &str) -> claims::Claims {
+    decode::<claims::Claims>(
+        token,
+        &DecodingKey::from_rsa_pem(key.as_bytes()).unwrap(),
+        &Validation::new(Algorithm::RS256),
+    )
+    .unwrap()
+    .claims
 }
